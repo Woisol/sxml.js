d2c8ce74e1f3acc13bd714ca7bab3b5c6251d8c6
Woisol-G <Woisol-G@outlook.com>
2026-05-03 19:01:22 +0800

    「 :rocket: 构建 / build 」: 使用 husky，配置 webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,39 @@
+const path = require('node:path');
+
+module.exports = {
+  mode: 'production',
+  target: 'node18',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: {
+      type: 'commonjs2',
+    },
+    clean: false,
+  },
+  devtool: false,
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+            compilerOptions: {
+              declaration: false,
+            },
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+};

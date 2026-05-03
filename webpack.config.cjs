135cdb36c0199a5fef198cc913af539dccd2e9d2
Woisol-G <Woisol-G@outlook.com>
2026-05-03 19:43:40 +0800

    「 :rocket: 构建 / build 」: 修改输出产物文件名、配置 GitHub action

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    filename: 'sxml.js',
     library: {
       type: 'commonjs2',
     },
